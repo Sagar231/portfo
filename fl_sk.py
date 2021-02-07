3ed1d6b6963dac66e8269d795423bbd277264d50
sagar <sagarfeb298@gmail.com>
2021-02-07 16:25:16 +0530

    added new files

---
+++ fl_sk.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect
+import csv
+app = Flask(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template("index.html")
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+def write_to_file(data):
+    with open("database.txt",mode='a') as f:
+        email = data['email']
+        subject=data['subject']
+        message=data['message']
+        file=f.write(f'\n{email},{subject},{message}')
+def write_to_csv(data):
+    with open("database.csv",mode='a') as database:
+        email = data['email']
+        subject=data['subject']
+        message=data['message']
+        csv_writer=csv.writer(database,delimiter=',')
+        csv_writer.writerow([email,subject,message])
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect("thanku.html")
+        except:
+            return 'something went wrong'
+    else:
+        return "something went wrong"
